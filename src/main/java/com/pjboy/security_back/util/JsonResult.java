e7d1f4dbefbfb595b224342596870d0bf95ec311
BLADE <1012582116@qq.com>
2020-04-22 18:20:25 +0800

    定义返回码

---
+++ src/main/java/com/pjboy/security_back/util/JsonResult.java
@@ -0,0 +1,10 @@
+package com.pjboy.security_back.util;
+
+/**
+ * @program: security_back
+ * @description: 统一返回实体
+ * @author: BLADE
+ * @create: 2020-04-22 18:19
+ **/
+public class JsonResult {
+}
